1e6a3504cd6f3dca61ac0160e6e6aace730af258
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-10-27 20:33:36 +0800

    feat(page): add login page structire

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,9 @@
-import "./index.scss";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
+import "./index.scss";
 import { useSetup } from "./hooks";
+import logoImg from "../../../public/logo.svg";
 
 type LoginProps = {};
 
@@ -8,7 +11,28 @@ function Login(props: LoginProps) {
   const {} = props;
   const {} = useSetup();
 
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <img src={logoImg} alt="logoImg" className="login-logo" />
+
+      <TextField
+        className="login-input m login-email"
+        label="邮箱"
+        variant="outlined"
+      />
+      <TextField
+        className="login-input m login-password"
+        label="密码"
+        variant="outlined"
+      />
+
+      <div className="spacer"></div>
+
+      <Button className="login-login_btn" variant="outlined">
+        登录
+      </Button>
+    </div>
+  );
 }
 
 export default Login;
