6853f04c6508e14de0f6249cda71199ef97b1744
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-10-27 21:03:50 +0800

    feat(style): change mui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
 import "./styles/index.scss";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#e79686",
+    },
+    secondary: {
+      main: "#a39391",
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
