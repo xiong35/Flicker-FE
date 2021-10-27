92f761e77d27a29e0ec84e11d75a3a0631e68be8
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-10-27 16:08:27 +0800

    feat(page): add router

---
+++ src/routes/index.ts
@@ -0,0 +1,49 @@
+import loadable from "@loadable/component";
+
+import { RouteConfig } from "react-router-config";
+
+export const routes: RouteConfig[] = [
+  {
+    path: "/login",
+    component: loadable(() => import("../pages/Login")),
+    exact: true,
+  },
+  {
+    path: "/register",
+    component: loadable(() => import("../pages/Register")),
+    exact: true,
+  },
+  {
+    path: "/home",
+    component: loadable(() => import("../pages/Home")),
+    exact: true,
+  },
+  {
+    path: "/deck/upload",
+    component: loadable(() => import("../pages/DeckUpload")),
+    exact: true,
+    strict: true,
+  },
+  {
+    path: "/deck/edit/:id",
+    component: loadable(() => import("../pages/DeckEdit")),
+    exact: true,
+  },
+  {
+    path: "/deck/:id",
+    component: loadable(() => import("../pages/DeckIntro")),
+    exact: true,
+    strict: true,
+  },
+  {
+    path: "/deck/:id/:question_id",
+    component: loadable(() => import("../pages/DeckQuestion")),
+    exact: true,
+    strict: true,
+  },
+  {
+    path: "/user",
+    component: loadable(() => import("../pages/User")),
+    exact: true,
+  },
+];
