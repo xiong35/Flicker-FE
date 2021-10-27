62195394f583a8bde3717e64ccf8574481382479
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-10-27 15:02:57 +0800

    build(other): fix build script

--- scripts/buildProd.sh
+++ scripts/buildProd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## 一键提交 dev, merge dev 到 master, 提交 master 的小脚本, 具体见下方注释 ##
+## 一键提交 dev, merge dev 到 main, 提交 main 的小脚本, 具体见下方注释 ##
 # 在 linux shell 下执行 `./scripts/buildProd.sh` 命令即可
 # windows 选手可以用 git bash 执行
 
@@ -11,11 +11,11 @@ git switch dev
 echo -e "\n### push DEV ###\n"
 git push
 
-echo -e "\n### switch to master ###\n"
-git switch master
-echo -e "\n### merge DEV into MASTER ###\n"
+echo -e "\n### switch to main ###\n"
+git switch main
+echo -e "\n### merge DEV into MAIN ###\n"
 git merge dev --no-ff
-echo -e "\n### push MASTER ###\n"
+echo -e "\n### push MAIN ###\n"
 git push
 
 echo -e "\n### switch back to DEV ###\n"
