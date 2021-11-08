b6052e516f10728e0c709bddf600578ff8aef49e
gu can <1154984091@qq.com>
2021-11-08 17:24:46 +0800

    feat(component): 修改DeckCard样式

--- src/components/DeckCard/index.tsx
+++ src/components/DeckCard/index.tsx
@@ -1,8 +1,10 @@
-import "./index.scss";
+import './index.scss';
 
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
-import { useSetup } from "./hooks";
+import Fire from '../../imgComponents/Fire';
+import Star from '../../imgComponents/star';
+import { useSetup } from './hooks';
 
 type DeckCardProps = {};
 
@@ -22,8 +24,14 @@ function DeckCard(props: DeckCardProps) {
         Laboriosam, qui?
       </div>
       <div className="deck_card-footer">
-        <div className="deck_card-footer-collected">123</div>
-        <div className="deck_card-footer-visited">456</div>
+        <div className="deck_card-footer-collected">
+          <Star className="deck_card-footer-icon" />
+          <span>{123}</span>
+        </div>
+        <div className="deck_card-footer-visited">
+          <Fire className="deck_card-footer-icon scale-110" />
+          <span>{456}</span>
+        </div>
         <div className="spacer"></div>
         <div className="deck_card-footer-time">2020/1/1 18:00</div>
       </div>
