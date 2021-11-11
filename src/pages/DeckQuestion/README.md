0fe3c1187fcb90f65f7f387388510fc529eaef82
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-11-11 14:43:07 +0800

    refactor(page): change question page to suport multiple types

---
+++ src/pages/DeckQuestion/README.md
@@ -0,0 +1,14 @@
+# 模块说明
+
+有多种答题模式, 其中相同之处的逻辑在 `index.tsx` 中, 主体部分写在 `components` 文件夹下
+
+目前支持的答题模式如下
+
+| 模式     | Comp 名字  | 对应 type   |
+| -------- | ---------- | ----------- |
+| 翻面卡片 | `FlipCard` | `flip-card` |
+| 填空题   | `FillIn`   | `fill-in`   |
+
+`hooks/useQuestionComp`会根据路由中提取出的 type(见上) query 来决定在主体部分渲染哪个组件并返回之
+
+上述组件中, 类名格式为: `dq-${type}`, 其中 `dq` 指 `DeckQuestion`
--- src/pages/DeckQuestion/index.tsx
+++ src/pages/DeckQuestion/index.tsx
@@ -14,43 +14,10 @@ function DeckQuestion(props: DeckQuestionProps) {
   const {} = useSetup();
   const { Comp } = useQuestionComp();
 
-  const [flipState, setFlipState] = useState<
-    "initial" | "flipping" | "flipped"
-  >("initial");
-
   return (
     <>
       <TheTopBar title="学习" />
       <Comp />
-      {/* <div className="deck_question">
-        <div
-          onClick={() => {
-            setFlipState("flipping");
-            setTimeout(() => setFlipState("flipped"), 300);
-          }}
-          className={`deck_question-card ${flipState}`}
-        >
-          <div className="deck_question-card-scroll">
-            {flipState === "flipped"
-              ? "answer"
-              : `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
-          inventore recusandae modi repudiandae accusantium quia tenetur, quasi
-          amet! Animi eius inventore itaque maiores molestiae commodi aliquid
-          distinctio tempora cumque nostrum?`}
-          </div>
-        </div>
-
-        <div className="deck_question-actions">
-          <div className="deck_question-actions-action">
-            <ArrowLeftCircle className="deck_question-actions-action-icon" />
-            上一条
-          </div>
-          <div className="deck_question-actions-action">
-            下一条
-            <ArrowLeftCircle className="deck_question-actions-action-icon rotate" />
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
