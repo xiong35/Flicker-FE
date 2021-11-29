fd9a8de47d7d1c29d5e2b61174d329aeb0f0f7af
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-11-29 18:41:40 +0800

    feat(model): change deck model

--- src/models/deck.ts
+++ src/models/deck.ts
@@ -28,6 +28,18 @@ export type Deck = {
 
   /** 包含的卡片 id */
   cards: CardID[];
+
+  /* 字符串，创建者id */
+  owner_id: string;
+  /* 字符串，创建者用户名 */
+  owner_name: string;
+  /* 字符串，收藏数量 */
+  favorite_count: string;
+  /* 字符串，访问数量 */
+  visit_count: string;
+  /* 整数，创建时间戳 */
+  create_time: number;
+  /* 整数，访问权限 */
 };
 
 export const initDeck: Deck = {
@@ -37,4 +49,9 @@ export const initDeck: Deck = {
   id: "",
   name: "",
   template: "",
+  create_time: Date.now(),
+  favorite_count: "0",
+  owner_id: "",
+  owner_name: "",
+  visit_count: "0",
 };
