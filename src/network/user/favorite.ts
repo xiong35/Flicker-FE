fa5a7740e4223001ab154aededeb3b30e8e18d07
gu can <1154984091@qq.com>
2021-12-16 15:15:55 +0800

    feat(network): add req about favorite

---
+++ src/network/user/favorite.ts
@@ -0,0 +1,30 @@
+import { Deck } from "../../models/deck";
+import _request from "../_request";
+
+export type ChangeFavoriteReqData = {
+  cardset_id: string; // 卡片集id
+  like: boolean; // 点击收藏按钮之前该卡片集是否被收藏，请求会将其取反
+};
+
+/**
+ * DESCRIPTION
+ * @returns
+ */
+export async function changeFavoriteReq(data: ChangeFavoriteReqData) {
+  const res = await _request({
+    url: "/favorite",
+    method: "POST",
+    data,
+  });
+
+  return res;
+}
+
+export async function getFavoriteReq() {
+  const res = await _request<Deck[]>({
+    url: "/favorite",
+    method: "GET",
+  });
+
+  return res;
+}
--- src/pages/DeckQuestion/components/Comments/index.tsx
+++ src/pages/DeckQuestion/components/Comments/index.tsx
@@ -4,12 +4,12 @@ import { useParams } from "react-router-dom";
 
 import { Avatar, TextField } from "@mui/material";
 
-import { useComments } from "./hooks/useComments";
-import { useAddComment } from "./hooks/useAddComment";
-import { CardID } from "../../../../models/card";
-import Send from "../../../../imgComponents/Send";
-import Discuss from "../../../../imgComponents/Discuss";
 import Empty from "../../../../components/Empty";
+import Discuss from "../../../../imgComponents/Discuss";
+import Send from "../../../../imgComponents/Send";
+import { CardID } from "../../../../models/card";
+import { useAddComment } from "./hooks/useAddComment";
+import { useComments } from "./hooks/useComments";
 
 type CommentProps = {
   id: CardID;
@@ -49,9 +49,6 @@ export default function Comments(props: CommentProps) {
             endAdornment: (
               <Send onClick={addComment} className="comments-input-icon" />
             ),
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") addComment();
           }}
         />
         <div className="comments-list">
