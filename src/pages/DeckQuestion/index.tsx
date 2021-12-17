460cd66e7b50651f6d9e8090508dd689376b1006
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-12-17 21:27:26 +0800

    fix(other): small fix

--- src/pages/DeckQuestion/index.tsx
+++ src/pages/DeckQuestion/index.tsx
@@ -3,6 +3,7 @@ import "./index.scss";
 import { useEffect, useRef } from "react";
 
 import TheTopBar from "../../components/TheTopBar";
+import { CardID } from "../../models/card";
 import { StudyStatus } from "../../models/study";
 import { addStudyRecord } from "../../utils/studyRecords/syncRecord";
 import { useAnimate } from "./hooks/useAnimate";
@@ -38,6 +39,12 @@ function DeckQuestion(props: DeckQuestionProps) {
     addStudyRecord({ card_id: cardQueue[2].id, cardset_id: deck.id, status });
   }
 
+  const idMap = cardQueue?.reduce<Record<CardID, number>>((map, card) => {
+    if (!card) return map;
+    map[card.id] = (map[card.id] || 0) + 1;
+    return map;
+  }, {});
+
   return (
     <div className="deck_question">
       <TheTopBar title="学习" />
@@ -45,8 +52,12 @@ function DeckQuestion(props: DeckQuestionProps) {
         {cardQueue?.map((card, i) => {
           if (!card)
             return <div className="deck_question-queue-item" key={i}></div>;
+
           return (
-            <div className="deck_question-queue-item" key={card.id}>
+            <div
+              className="deck_question-queue-item"
+              key={idMap?.[card.id] === 1 ? card.id : `${card.id}${i}`}
+            >
               <Comp
                 card={card}
                 switchCard={switchWithAnim}
--- src/pages/DeckQuestion/utils/recommand.ts
+++ src/pages/DeckQuestion/utils/recommand.ts
@@ -5,25 +5,25 @@ import { Deck, DeckID } from "../../../models/deck";
 import { getCards } from "../../../network/card/getCards";
 import { getRecordByDeckIDFromLocal } from "../../../utils/studyRecords/syncRecord";
 
-export async function getNextNQuestion(deck: Deck, count = 10) {
+export async function getNextNQuestion(deck: Deck, count = 5) {
   let idsToFetch: DeckID[] = [];
   const recordMap = getRecordByDeckIDFromLocal(deck.id);
 
   function compareRecords(card1: CardID, card2: CardID) {
     const record1 = recordMap!.recordMap[card1];
     const record2 = recordMap!.recordMap[card2];
-    // 优先判断是否已经学过(即有记录)
+    // 1. 判断是否已经学过(即有记录)
     if (!(record1 && record2)) {
-      // 有人没学过, 他应该优先学
+      // 没有学习记录的应该优先学
       if (!record1) return -1;
       if (!record2) return 1;
     }
 
-    // 判断是否不会, 不会的优于会的
+    // 2. 判断是否不会, 不会的优于会的
     if (record1.status === 1) return 1;
     if (record2.status === 1) return -1;
 
-    // 再判断学习时间, 上次学习时间远的优于学习时间近的
+    // 3. 判断学习时间, 上次学习时间远的优于学习时间近的
     return record1.last_study - record2.last_study;
   }
 
